9e087971838fba0bf26bb1a535b37819161b1f4e
DiogoFlowers <diogoflores2009@gmail.com>
2025-07-17 18:52:54 -0300

    first commit

---
+++ src/test/java/com/managerfinance/managerfinance/ManagerfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managerfinance.managerfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
